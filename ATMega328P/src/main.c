e88563c242c0eb0283ec72066040a67536488f3a
nachito <igchantiri99@gmail.com>
2026-02-10 15:21:27 -0300

    codigo

--- ATMega328P/src/main.c
+++ ATMega328P/src/main.c
@@ -1,95 +1,120 @@
 /*
- * TP0 Control III.c
+ * TA3 Control III.c
  *
- * Created: 24/8/2025 14:55:34
+ * Created: 1/12/2025 
  * Author : nacho
  */ 
 
-#include "funciones.h"
 #include "UART.h"
+#include "funciones_generales.h"
+#include "funciones_identificacion.h"
+#include "funciones_control.h"
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <stdio.h>
 
-volatile uint8_t contador_PRBS = 0;
-volatile uint16_t tension_entrada;
-volatile uint16_t tension_filtrada = 0;
-volatile uint16_t registro_LFSR = 0;
-volatile uint8_t flag_test_PRBS = 0;
-volatile uint16_t N = 0;
+// Variables interrupciones
+volatile uint8_t flag_muestreo = 0;
+volatile uint8_t flag_perturbacion = 0;
+volatile uint8_t contador_muestreo = 0;
+volatile uint16_t contador_referencia = 0;
+volatile uint16_t contador_perturbacion = 0;
 
-uint8_t flag_lectura_ADC = 0;
-
-#define referencia 3000 // (En milivoltios)
-
-ISR(TIMER1_OVF_vect) {
-	
-	 flag_lectura_ADC = 1; // Esta interrupcion no se usa en este TA.
-
-};
-
-ISR(PCINT2_vect) { 
-	
-    // Código que se ejecuta cuando se dispara la interrupción en PD4 (Switch 1)
-
-    if (PIND & (1 << PD4)) {
-
-		terminar_test_PRBS(); 
-
-		test_escalon(1000, 4000); // Test de respuesta al escalon: de 1V a 4V en 5 segundos, sale por PB1.
-
-    }
-};
-
-ISR(INT1_vect)
+ISR(TIMER2_COMPA_vect) // Interrupción cada 1 ms
 {
-    // Código que se ejecuta cuando se dispara la interrupción en PD3  (Switch 2)
+    if (++contador_muestreo >= 30) // Muestreo
+    {
+        contador_muestreo = 0;
+		flag_muestreo = 1;
+		// PORTB ^= (1 << PB0);
 
-	comenzar_test_PRBS();
+	}
 
-}
+    if (++contador_referencia >= 10000) 
+    {
+        contador_referencia = 0;
+		contador_perturbacion = 0;
+		flag_perturbacion = 1;
+		cambiar_referencia(1000, 4000); // Cambia la referencia entre 1V y 4V cada 10 segundos
+		delta = (referencia == 1000) ? 0 : 1000;
+		PORTB ^= (1 << PB0);
+	}
 
-ISR(TIMER0_COMPA_vect)
-{
-    // Código que se ejecuta a 61 Hz
-
-	contador_PRBS++;
-
-	if (contador_PRBS >= 4) { // Se ejecuta aproximadamente cada 0.066 segundos
-
-		N++;
-		if (N == 2047) {
-
-			terminar_test_PRBS();
-
-		} else {
-		
-			contador_PRBS = 0;
-			actualizar_PWM_PRBS();
+	if (flag_perturbacion)
+	{
+		if (++contador_perturbacion >= 5000)  // Cada 5 segundos, se activa o desactiva la perturbación.
+		{
+			flag_perturbacion = 0;
+			delta = (delta == 0) ? 1000 : 0; // Cambia entre 0 y 1V de perturbación.
 
 		}
 	}
 }
 
+ISR(PCINT2_vect) { // Interrupción cuando se presiona el switch 1 (PD4)
+	
+    if (PIND & (1 << PD4)) {
+
+		perturbacion_activada = 1;
+		delta = 3500; 
+		PORTB |= (1 << PB0); // Enciende LED indicador
+}
+}
+
+ISR(INT1_vect) // Interrupción cuando se presiona el switch 2 (PD3)
+{
+
+		perturbacion_activada = 0;
+		delta = 0; 
+		PORTB &= ~(1 << PB0); // Apaga LED indicador
+}
+
+ISR(TIMER0_COMPA_vect) // Código que se ejecuta a 61 Hz (cada 16.39 ms)
+{
+
+	// contador_PRBS++;
+
+	// if (contador_PRBS >= 4) { 
+
+	// 	// Cada 4 interrupciones de Timer0 (osea, cada 65.56 ms), se generan nuevos bits del PRBS.
+
+	// 	N++; // N lleva cuenta de la cantidad de bits generados en el test de PRBS. Termina cuando N = 2047.
+
+	// 	if (N == 2047) {
+
+	// 		terminar_test_PRBS();
+
+	// 	} else {
+		
+	// 		contador_PRBS = 0;
+	// 		actualizar_PWM_PRBS();
+
+	// 	}
+	// }
+}
+
 	int main(void)
 	{
+		
 		setup_ADC();
 		setup_PWM();
 		setup_SWITCHS();
 		setup_LFSR();
 		USART_init();
 		sei();
-
-		// ---- RESPUESTA AL ESCALON ---- //
-
-
+		
 		while (1)
 		{
 
-		
-		} 
+			if (flag_muestreo) {
 
-    return 0;
-}
+				flag_muestreo = 0;
+				aplicar_control_MPC(referencia);
+			}
+		}
+
+		return 0;
+	}
 
